8d160c9b125322390b8c176c9797d8e376c6ff3b
saich-bh2024 <akhileshchunduri@gmail.com>
2024-10-03 18:49:41 +0530

    for loop

--- py_durga/otp.py
+++ py_durga/otp.py
@@ -11,4 +11,9 @@ print(randint(0,9), randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(
 
 print(randint(0,9), randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9), sep="-" )
 
+#==========================================
+
+for i in range(10):
+    print(randint(0,9), randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9), sep="-" )
+
 
