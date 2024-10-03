8e5924aa6db09da18b5635e526b4372ff179ddd1
saich-bh2024 <akhileshchunduri@gmail.com>
2024-10-03 18:36:53 +0530

    separtor in print stmnt

--- py_durga/otp.py
+++ py_durga/otp.py
@@ -1,3 +1,14 @@
 import random
 from random import randint
 print(randint(0,9))
+
+
+#=======================================
+from random import randint
+print(randint(0,9), randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9) )
+
+#=======================================
+
+print(randint(0,9), randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9), sep="-" )
+
+
