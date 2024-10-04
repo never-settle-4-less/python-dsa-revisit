84222288564dc1c779267ba42d27c3487d635e12
saich-bh2024 <akhileshchunduri@gmail.com>
2024-10-04 22:08:13 +0530

    identifiers

---
+++ py_durga/2_indentifiers.py
@@ -0,0 +1,38 @@
+x=10
+
+def f():
+    print('f is a function')
+
+class Test:
+    def m1(self):
+        print('m1 is a method')
+
+#function can be defined outside, methods are 
+#nothing but fuctions defined inside a class
+
+#independent fuctions are the ones we typically define
+#functions defined inside a class are callled  methods 
+
+# a name in python program is called an identifier 
+
+#cash = 1000 (valid)
+#ca$h = 1000 (invalid)
+
+cash = 1000
+#ca$h = 900
+cash_amnt = 900
+
+# $ is not allowed, since python is used as scripting language and $ is often used in scripting 
+
+total = 89
+total 
+
+TOTAL = 90909
+TOTAL
+
+jfgjjjegkjrfgkjerhgkjrqhfhkjefdsbfvjdffbvjfdbvjkegfbvjsdbvjkdfbvdfbvefjvbdjsfkvgevbefjvbdfjvberjfvbkefvbvfjkvberjkvbjfkvbfdjvbefjkvbjfdvbfjhvbefjkgberjkfh = 90
+
+print(jfgjjjegkjrfgkjerhgkjrqhfhkjefdsbfvjdffbvjfdbvjkegfbvjsdbvjkdfbvdfbvefjvbdjsfkvgevbefjvbdfjvberjfvbkefvbvfjkvberjkvbjfkvbfdjvbefjkvbjfdvbfjhvbefjkgberjkfh)
+
+#no lenth limit, but not recommeneded to take large length identifiers 
+
